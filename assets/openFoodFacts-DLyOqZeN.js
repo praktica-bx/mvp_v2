f5f0e66e9707f32946190cd04c01dee99cff3732
che99ddz <johannes.lange@sus.no>
2026-05-10 00:08:33 +0200

    Updates

--- assets/openFoodFacts-DLyOqZeN.js
+++
@@ -1,1 +0,0 @@
-import{n as e,r as t,t as n}from"./index-Cy83PuRQ.js";export{e as default,n as lookupByBarcode,t as searchProducts};
---
+++ assets/openFoodFacts-DSrRg_TZ.js
@@ -0,0 +1,1 @@
+import{n as e,r as t,t as n}from"./index-CTUVfm3Y.js";export{e as default,n as lookupByBarcode,t as searchProducts};
