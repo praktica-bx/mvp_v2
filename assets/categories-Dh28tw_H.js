c693009e8c964a53c90754c552e64a361b32005c
che99ddz <johannes.lange@sus.no>
2026-05-24 20:58:22 +0200

    Updates

--- assets/categories-Dh28tw_H.js
+++
@@ -1,1 +0,0 @@
-import{a as e,i as t,o as n,s as r}from"./index-CVDzJQ9f.js";export{t as CATEGORY_QUANTITY_MAP,e as SUPPLY_CATEGORIES,n as getCategoryLabel,r as getDsbCategory};
--- assets/openFoodFacts-wHxna1DV.js
+++
@@ -1,1 +0,0 @@
-import{n as e,r as t,t as n}from"./index-CVDzJQ9f.js";export{e as default,n as lookupByBarcode,t as searchProducts};
